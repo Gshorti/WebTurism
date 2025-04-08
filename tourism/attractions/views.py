33178ae0761d319ba99838ae59476f3c0df32cfa
gshorti <ilazajkin52969@gmail.com>
2025-04-08 13:44:46 +0300

    Create views for models

--- tourism/attractions/views.py
+++ tourism/attractions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import AttractionSerializer
+from .models import Attraction
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+class AttractionViewSet(ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
+    queryset = Attraction.objects.all()
+    serializer_class = AttractionSerializer
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['name', 'price']
+    search_fields = ['name', 'description', 'address', 'price']
+    ordering_fields = ['id']
--- tourism/objects/views.py
+++ tourism/objects/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ObjectSerializer
+from .models import Objects
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+class AttractionViewSet(ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
+    queryset = Objects.objects.all()
+    serializer_class = ObjectSerializer
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['name', 'price']
+    search_fields = ['name', 'description', 'address', 'price']
+    ordering_fields = ['id']
