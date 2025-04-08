db6cb8bc6d1976ca5bd65ab59eeac76151654d13
gshorti <ilazajkin52969@gmail.com>
2025-04-08 15:44:36 +0300

    Ura

--- tourism/attractions/filters.py
+++ tourism/attractions/filters.py
@@ -1,11 +1,14 @@
 def TagFilter(request, response):
     tags = request.GET["tags"]
-    tags = tags.split(',')
-    res = []
-    for item in response.data:
-        for tag in item["tags"]:
-            for filter_tag in tags:
-                if filter_tag == tag:
-                    if item not in res:
-                        res.append(item)
-    return res
+    if tags != "NaN":
+        tags = tags.split(',')
+        res = []
+        for item in response.data:
+            for tag in item["tags"]:
+                for filter_tag in tags:
+                    if filter_tag == tag:
+                        if item not in res:
+                            res.append(item)
+        return res
+    else:
+        return response.data
