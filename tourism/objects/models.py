d33dd554641467873cc7aa5847b90c14541fbaf3
gshorti <ilazajkin52969@gmail.com>
2025-04-08 16:20:49 +0300

    Change time to char

--- tourism/objects/models.py
+++ tourism/objects/models.py
@@ -9,4 +9,4 @@ class Objects(models.Model):
     image = models.FileField(upload_to="images",null=False)
     tags = models.JSONField(null=False, blank=True, default=list)
     price = models.IntegerField(blank=True)
-    time = models.DateTimeField(null=True, blank=False)
+    time = models.CharField(max_length=100)
