8aad34db8a0d35051c5b310a418841e681f82631
gshorti <ilazajkin52969@gmail.com>
2025-04-08 14:18:41 +0300

    Fix serializers.py for objects and attractions.

--- tourism/attractions/serializers.py
+++ tourism/attractions/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework.serializers import ModelSerializer
+from .models import Attraction
 
 class AttractionSerializer(ModelSerializer):
 
     class Meta:
-
+        model = Attraction
         fields = '__all__'
--- tourism/objects/serializers.py
+++ tourism/objects/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer
+from .models import Objects
 
 class ObjectSerializer(ModelSerializer):
 
     class Meta:
+        model = Objects
         fields = '__all__'
