7ff9babfcd7f2ad931866f764eab6b71ea6f8bd9
gshorti <ilazajkin52969@gmail.com>
2025-04-08 12:10:35 +0300

    CORS ALLOW ALL ORIGINS

--- tourism/tourism/settings.py
+++ tourism/tourism/settings.py
@@ -134,11 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "https://kptube.netlify.app",
-    "http://localhost:4200",
-    "http://127.0.0.1:4200"
-]
+CORS_ALLOW_ALL_ORIGINS =True
 CORS_ALLOW_HEADERS = [
 'accept',
 'accept-encoding',
