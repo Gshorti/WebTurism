a1ea4c36d06363d2c0e58c52ad7dc241aa99c0c0
gshorti <ilazajkin52969@gmail.com>
2025-04-08 15:27:42 +0300

    Krutoy apelsin

--- tourism/attractions/views.py
+++ tourism/attractions/views.py
@@ -18,5 +18,11 @@ class AttractionViewSet(ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
-        response = TagFilter(request, response)
+        tags = request.GET["tags"]
+        tags = tags.split(',')
+        res = []
+        for item in response:
+            for tag in item["tags"]:
+                res.append(tag)
+        response.data.append(res)
         return response
