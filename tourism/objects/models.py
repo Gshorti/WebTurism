31033b5909bece6f855b0c3f29f170adf37475a4
gshorti <ilazajkin52969@gmail.com>
2025-04-08 12:04:05 +0300

    Create Object models and add models to the admin panel.

--- tourism/objects/models.py
+++ tourism/objects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Objects(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False)
+    address = models.CharField(max_length=300, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    map = models.JSONField(null=False, default=dict, blank=True)
+    image = models.FileField(upload_to="images",null=False)
+    tags = models.JSONField(null=False, blank=True, default=list)
+    price = models.IntegerField(blank=True)
