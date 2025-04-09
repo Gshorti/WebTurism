736e48c874aee5602ddb1a231600a0ba69bae7aa
gshorti <ilazajkin52969@gmail.com>
2025-04-09 12:58:18 +0300

    Add __str__ to Compilation model.

--- tourism/compilations/models.py
+++ tourism/compilations/models.py
@@ -4,4 +4,6 @@ class Compilation(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     data = models.JSONField(null=False, blank=False, default=list)
-    
+
+    def __str__(self):
+        return self.name
